5fa33a44dd519dbafcae89f03cba17ab46ddc2ed
kalmegati <kalmegati@gmail.com>
2020-10-13 21:56:28 -0400

    added static classes list

--- options.py
+++ options.py
@@ -1,11 +1,17 @@
 def ancestries():
-    ances = ["Dwarf", "Elf", "Gnome", "Goblin", "Halfling", "Human", "Orc"]
-    return ances
+    ancs = ["Dwarf", "Elf", "Gnome", "Goblin", "Halfling", "Human", "Orc"]
+    return ancs
 
 print(ancestries())
 
 def backgrounds():
-    backs = ["Acolyte", "Artisan", "Hunter", "Performer", "Scholar", "Soldier"]
-    return backs
+    bacs = ["Acolyte", "Artisan", "Hunter", "Performer", "Scholar", "Soldier"]
+    return bacs
 
 print(backgrounds())
+
+def classes():
+    clas = ["Alchemist", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Wizard"]
+    return clas
+
+print(classes())
