30287f3523565cf465f96283b0f7185752edb889
kalmegati <kalmegati@gmail.com>
2020-10-15 11:12:20 -0400

    removed printing from options

--- options.py
+++ options.py
@@ -2,16 +2,10 @@ def ancestries():
     ancs = ["Dwarf", "Elf", "Gnome", "Goblin", "Halfling", "Human", "Orc"]
     return ancs
 
-print(ancestries())
-
 def backgrounds():
     bacs = ["Acolyte", "Artisan", "Hunter", "Performer", "Scholar", "Soldier"]
     return bacs
 
-print(backgrounds())
-
 def classes():
     clas = ["Alchemist", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Wizard"]
     return clas
-
-print(classes())
