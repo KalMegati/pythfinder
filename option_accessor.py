56465166c0d8cdb355e0a29ebc9753b9dab067c4
kalmegati <kalmegati@gmail.com>
2020-10-16 11:28:59 -0400

    successfully installed and imported numpy

--- option_accessor.py
+++ option_accessor.py
@@ -1,7 +1,8 @@
 #import matplotlib.pyplot as plt
-# import numpy as np
+import numpy as np
 import options
 
 backgrounds = options.backgrounds()
 
-print(backgrounds)
+my_arr = np.array([2,4,6,8])
+
