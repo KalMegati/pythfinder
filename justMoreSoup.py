25a2c4fec102a11a519f842833b62081aecaa390
kalmegati <kalmegati@gmail.com>
2020-10-26 22:20:16 -0400

    removed commented-out code from testing files

--- justMoreSoup.py
+++ justMoreSoup.py
@@ -1,25 +1,3 @@
-# import requests
-# from bs4 import BeautifulSoup
-
-
-# headers = {
-#     'Access-Control-Allow-Origin': '*',
-#     'Access-Control-Allow-Methods': 'GET',
-#     'Access-Control-Allow-Headers': 'Content-Type',
-#     'Access-Control-Max-Age': '3600',
-#     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-#     }
-
-# url = "https://2e.aonprd.com/Classes.aspx"
-# req = requests.get(url, headers)
-# soup = BeautifulSoup(req.content, 'html.parser')
-# classes = []
-# for link in soup.find_all('a'):
-#     if "Classes.aspx?ID" in link.get('href'):
-#         classes.append(link.text)
-
-# print(classes)
-
 import options
 
 print(options.backgrounds())
