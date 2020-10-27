64c9bc8ccdaf4e6c6a8aabdbca470546da057b72
kalmegati <kalmegati@gmail.com>
2020-10-26 22:28:57 -0400

    improved formatting/clarity of UI instructions

--- option_accessor.py
+++ option_accessor.py
@@ -6,6 +6,6 @@ classes = options.classes()
 for id, clas in enumerate(classes): 
     print (f'{id+1}) {clas}')
 
-choice = input("select a number")
+choice = input("select a number \n")
 
 print(classes[int(choice)-1])
