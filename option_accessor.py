e7b4cf5f1db9e0ad8fd45f472372a29fc9bb9dab
kalmegati <kalmegati@gmail.com>
2020-10-26 23:08:27 -0400

    selecting a class from the list now displays scraped class summary

--- option_accessor.py
+++ option_accessor.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
 import options
 
 classes = options.classes()
@@ -8,4 +11,20 @@ for id, clas in enumerate(classes.keys()):
 
 choice = input("select a number \n")
 
-print(classes[list(classes.keys())[int(choice)-1]])
+op_link = classes[list(classes.keys())[int(choice)-1]]
+
+headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Max-Age': '3600',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+    }
+
+def summarizer(link):
+    url = f"https://2e.aonprd.com/{link}"
+    req = requests.get(url, headers)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    return soup.find_all("i")[1].text
+
+print(summarizer(op_link))
